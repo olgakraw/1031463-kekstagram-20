6fd6430c600d418678eefdd592c0a9489616b9f4
olga <kontrolle@yandex.ru>
2020-07-04 22:54:34 +0600

    Добавляет задание 20. Личный проект: перламутровые пуговицы (часть 1)

--- js/data.js
+++ js/data.js
@@ -11,13 +11,103 @@
     return element;
   };
 
-  window.download(function (newPosts) {
-    var fragment = document.createDocumentFragment();
-    var postList = document.querySelector('.pictures');
+  var fragment = document.createDocumentFragment();
+  var postList = document.querySelector('.pictures');
 
+  window.download(function (newPosts) {
     newPosts.forEach(function (item) {
       fragment.appendChild(createNewPost(item));
       postList.appendChild(fragment);
     });
+
+    window.postsArray = Array.from(newPosts);
+
   }, function () {});
+
+  window.updateArrayDiscussed = function (array) {
+
+    window.filterDiscussedArray = array.sort(function (post1, post2) {
+      if (post1.comments.length < post2.comments.length) {
+        return 1;
+      }
+      if (post1.comments.length > post2.comments.length) {
+        return -1;
+      }
+      return 0;
+    });
+  };
+
+  window.updateArrayRandom = function (array) {
+
+    window.filterRandomArray = [];
+    for (var i = 0; i < 10; i++) {
+      var index = Math.floor(Math.random() * array.length);
+      window.filterRandomArray.push(array[index]);
+      array.splice(index, 1);
+    }
+  };
+
+  var pictures = [];
+
+  var removePosts = function () {
+
+    pictures = Array.from(postList.querySelectorAll('.picture'));
+
+    pictures.forEach(function (it) {
+      postList.removeChild(it);
+    });
+  };
+
+  var renderPosts = function (array) {
+
+    Array.prototype.push.apply(pictures, array);
+
+    array.forEach(function (item) {
+      fragment.appendChild(createNewPost(item));
+      postList.appendChild(fragment);
+    });
+  };
+
+  var defaultFilter = document.querySelector('#filter-default');
+  var discussedFilter = document.querySelector('#filter-discussed');
+  var randomFilter = document.querySelector('#filter-random');
+
+  discussedFilter.addEventListener('click', function () {
+
+    defaultFilter.classList.remove('img-filters__button--active');
+    randomFilter.classList.remove('img-filters__button--active');
+    discussedFilter.classList.add('img-filters__button--active');
+
+    window.debounce(function () {
+      removePosts();
+      window.updateArrayDiscussed(window.postsArray.slice(0));
+      renderPosts(window.filterDiscussedArray);
+    })();
+  });
+
+
+  randomFilter.addEventListener('click', function () {
+
+    defaultFilter.classList.remove('img-filters__button--active');
+    discussedFilter.classList.remove('img-filters__button--active');
+    randomFilter.classList.add('img-filters__button--active');
+
+    window.debounce(function () {
+      removePosts();
+      window.updateArrayRandom(window.postsArray.slice(0));
+      renderPosts(window.filterRandomArray);
+    })();
+  });
+
+  defaultFilter.addEventListener('click', function () {
+
+    discussedFilter.classList.remove('img-filters__button--active');
+    randomFilter.classList.remove('img-filters__button--active');
+    defaultFilter.classList.add('img-filters__button--active');
+
+    window.debounce(function () {
+      removePosts();
+      renderPosts(window.postsArray);
+    })();
+  });
 })();
--- js/download.js
+++ js/download.js
@@ -6,6 +6,7 @@
     OK: 200
   };
   var TIMEOUT_IN_MS = 10000;
+  var filterMessage = document.querySelector('.img-filters');
 
   window.download = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
@@ -14,8 +15,10 @@
     xhr.addEventListener('load', function () {
       if (xhr.status === StatusCode.OK) {
         onSuccess(xhr.response);
+        filterMessage.classList.remove('img-filters--inactive');
       } else {
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        filterMessage.classList.add('img-filters--inactive');
       }
     });
     xhr.addEventListener('error', function () {
