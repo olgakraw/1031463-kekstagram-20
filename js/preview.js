da0b30b5f418339f5a1e6b8701f45f1c4fb73ac6
olga <kontrolle@yandex.ru>
2020-07-20 22:14:57 +0600

    new

--- js/preview.js
+++ js/preview.js
@@ -6,13 +6,37 @@
   var MIN_SCALE_VALUE = 25;
   var SCALE_STEP = 25;
 
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
   var EFFECTS = ['none', 'chrome', 'sepia', 'marvin', 'phobos', 'heat'];
 
   var effectDepth = document.querySelector('.effect-level__depth');
   var effectPin = document.querySelector('.effect-level__pin');
   var effectValue = document.querySelector('.effect-level__value');
   var imagePreview = document.querySelector('.img-upload__preview');
+  var preview = document.querySelector('.img-upload__preview img');
   var imageUploadEffects = document.querySelector('.img-upload__effects');
+
+  var fileChooser = document.querySelector('#upload-file');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
 
   var filterChangeHandler = function (evt) {
 
@@ -171,4 +195,5 @@
   scaleUp.addEventListener('click', function () {
     makeBigger();
   });
+
 })();
